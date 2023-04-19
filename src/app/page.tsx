99ef5a8f1c81a522a5201c9d5ae2e859f43c546b
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-04-19 11:10:35 -0300

    feat: create page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 export default function Home() {
   return (
-    <main>Clone Spotify</main>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-64 bg-zinc-950 p-6">
+          sidebar
+        </aside>
+        <main className="flex-1 p-6">
+          main
+        </main>
+      </div>
+      <div>
+        <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+          footer
+        </footer>
+      </div>
+    </div>
   )
 }
