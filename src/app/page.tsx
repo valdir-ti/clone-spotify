2cdcdd9945f795ae2473f3fd33734549156de012
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-04-19 11:28:40 -0300

    feat: create sidebar navigation items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { Home as HomeIcon, Search, Library } from 'lucide-react'
+
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-64 bg-zinc-950 p-6">
-          sidebar
+        <aside className="w-72 bg-zinc-950 p-6">
+          <nav className="flex flex-col space-y-5">
+            <a href="" className='flex items-center gap-4 text-sm font-semibold text-zinc-200'>
+              <HomeIcon />
+              Home
+            </a>
+            <a href="" className='flex items-center gap-4 text-sm font-semibold text-zinc-200'>
+              <Search />
+              Search
+            </a>
+            <a href="" className='flex items-center gap-4 text-sm font-semibold text-zinc-200'>
+              <Library />
+              Your Library
+            </a>
+          </nav>
         </aside>
         <main className="flex-1 p-6">
           main
