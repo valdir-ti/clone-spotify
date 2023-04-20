5c576cd17b9b3c8a002d8561a912a7b9eebee832
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-04-20 14:23:22 -0300

    feat: create footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from 'lucide-react'
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Shuffle, SkipBack, Play, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume2, Maximize2 } from 'lucide-react'
 import Album from './components/Album'
 import Card from './components/Card'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -75,8 +76,44 @@ export default function Home() {
         </main>
       </div>
       <div>
-        <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-          footer
+        <footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
+          <div className='flex items-center gap-3'>
+            <Image src='/album.jpg' alt='album' width={56} height={56} />
+            <div className='flex flex-col'>
+              <strong className='font-normal'>Rope</strong>
+              <span className='text-xs text-zinc-400'>Mettalica</span>
+            </div>
+          </div>
+          <div className='flex flex-col items-center gap-2'>
+            <div className='flex items-center gap-6'>
+              <Shuffle size={20} className='text-zinc-200'/>
+              <SkipBack size={20} className='text-zinc-200'/>
+              <button className='w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black ml-auto'>
+                <Play />
+              </button>
+              <SkipForward size={20} className='text-zinc-200'/>
+              <Repeat size={20} className='text-zinc-200'/>
+            </div>
+            <div className='flex items-center gap-2'>
+              <span className='text-xs text-zinc-500'>0:31</span>
+              <div className='h-1 rounded-full w-96 bg-zinc-600'>
+                <div className='bg-zinc-200 w-32 h-1 rounded-full'></div>
+              </div>
+              <span className='text-xs text-zinc-500'>2:16</span>
+            </div>
+          </div>
+          <div className='flex items-center gap-4'>
+            <Mic2 size={16} />
+            <LayoutList size={16}/>
+            <Laptop2 size={16}/>
+            <div className='flex items-center gap-2'>
+              <Volume2 size={16}/>
+              <div className='h-1 rounded-full w-24 bg-zinc-600'>
+                <div className='bg-zinc-200 w-12 h-1 rounded-full'></div>
+              </div>
+            </div>
+            <Maximize2 size={16}/>
+          </div>
         </footer>
       </div>
     </div>
